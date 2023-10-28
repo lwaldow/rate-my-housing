b0418bca991652d0b69846327e7f1cf5b4d589db
piyush2003m <piyush2003m@gmail.com>
2023-10-28 18:23:34 -0400

    git ignore

--- rate-my-housing/server/server.js
+++ rate-my-housing/server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = 8080;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.json({message: "hello world"});
