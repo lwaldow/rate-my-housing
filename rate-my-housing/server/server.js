0603fc042796e71c371fa3c1e252c0733cb816de
piyush2003m <piyush2003m@gmail.com>
2023-10-29 23:12:21 -0400

    minor changes

--- rate-my-housing/server/server.js
+++ rate-my-housing/server/server.js
@@ -1,27 +1,25 @@
-import { Sequelize, DataTypes } from 'sequelize';
-
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
 
-const Sequelize = require("sequelize");
-const sequelize = new Sequelize(
- process.env.POSTGRESQL_DB,
- process.env.POSTGRESQL_DB_USER, 
- process.env.POSTGRESQL_DB_USER, 
-  {
-    host: process.env.POSTGRESQL_DB_HOST, 
-    dialect: 'postgres'
-  }
-);
+// const Sequelize = require("sequelize");
+// const sequelize = new Sequelize(
+//  process.env.POSTGRESQL_DB,
+//  process.env.POSTGRESQL_DB_USER, 
+//  process.env.POSTGRESQL_DB_USER, 
+//   {
+//     host: process.env.POSTGRESQL_DB_HOST, 
+//     dialect: 'postgres'
+//   }
+// );
 
 
-sequelize.authenticate().then(() => {
-   console.log('ORM Connection has been established successfully.');
-}).catch((error) => {
-   console.error('ORM Unable to connect to the database: ', error);
-});
+// sequelize.authenticate().then(() => {
+//    console.log('ORM Connection has been established successfully.');
+// }).catch((error) => {
+//    console.error('ORM Unable to connect to the database: ', error);
+// });
 
 
 app.get("/", (req, res) => {
