39e389cf68b7dae1ea41ce897aa4703a41a40e0f
marianaj15 <144620785+marianaj15@users.noreply.github.com>
2023-11-01 11:44:20 -0400

    sign up modal test

--- rate-my-housing/components/Navbar.tsx
+++ rate-my-housing/components/Navbar.tsx
@@ -18,9 +18,9 @@ export default function Navbar() {
                 <div className="flex justify-between items-center">
                     <Link href="/" className={playfair.className + " flex items-center"}>
                         <img
-                        src="/favicon.ico"
-                        alt="Logo"
-                        className="w-12 h-12 mr-0.1" // Adjust the width and height as needed
+                            src="/favicon.ico"
+                            alt="Logo"
+                            className="w-12 h-12 mr-0.1" // Adjust the width and height as needed
                         />
                         <span className="text-self-center whitespace-nowrap text-3xl font-extrabold text-maroon">RateMy</span>
                         <span className="text-self-center whitespace-nowrap text-3xl font-extrabold text-slate-700">Housing</span>
@@ -28,7 +28,7 @@ export default function Navbar() {
                     <div>
                         <Link href="/about" className="text-self-center text-1xl font-bold text-maroon underline mr-2">About</Link>
                         <a onClick={handleOpen} className=" cursor-pointer text-self-center text-1xl font-bold text-maroon underline mr-2">Log In</a>
-                        <Link href="/signup" className="text-self-center text-1xl font-bold text-maroon underline">Sign Up</Link>
+                        <a onClick={handleOpen} className="text-self-center text-1xl font-bold text-maroon underline">Sign Up</a>
                     </div>
                 </div>
             </nav>
---
+++ rate-my-housing/components/SignUp.tsx
@@ -0,0 +1,138 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button, { ButtonProps } from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Grid from '@mui/material/Grid';
+import Link from '@mui/material/Link';
+import Card from '@mui/material/Card';
+import { styled } from '@mui/material';
+
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+};
+
+interface SignInModalProps {
+    open: boolean;
+    handleClose: (event: object, reason: string) => void;
+}
+
+export default function SignInModal({ open, handleClose }: SignInModalProps) {
+
+
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            password: data.get('password'),
+        });
+    };
+
+    return (
+        <div>
+            <Modal
+                keepMounted
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="keep-mounted-modal-title"
+                aria-describedby="keep-mounted-modal-description"
+            >
+                <Container maxWidth="sm" disableGutters>
+                    <Card
+                        sx={{
+                            padding: 6,
+                            marginTop: 8,
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+                            <LockOutlinedIcon />
+                        </Avatar>
+                        <Typography component="h1" variant="h5">
+                            Sign up
+                        </Typography>
+                        <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                            <Grid container spacing={2}>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        autoComplete="given-name"
+                                        name="firstName"
+                                        required
+                                        fullWidth
+                                        id="firstName"
+                                        label="First Name"
+                                        autoFocus
+                                    />
+                                </Grid>
+                                <Grid item xs={12} sm={6}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="lastName"
+                                        label="Last Name"
+                                        name="lastName"
+                                        autoComplete="family-name"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        id="email"
+                                        label="Email Address"
+                                        name="email"
+                                        autoComplete="email"
+                                    />
+                                </Grid>
+                                <Grid item xs={12}>
+                                    <TextField
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="Password"
+                                        type="password"
+                                        id="password"
+                                        autoComplete="new-password"
+                                    />
+                                </Grid>
+                            </Grid>
+                            <Button
+                                color="primary"
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2 }}
+                            >
+                                Sign Up
+                            </Button>
+                            <Grid container justifyContent="flex-end">
+                                <Grid item>
+                                    <Link href="/signin" variant="body2">
+                                        Already have an account? Sign in
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Box>
+                    </Card>
+                </Container>
+            </Modal>
+        </div>
+    );
+}
