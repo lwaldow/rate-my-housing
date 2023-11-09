180d70283aaa8d0c6e970fd0cf3cfce0dd00a76d
Luke Waldow <lwaldow@umass.edu>
2023-11-09 13:33:49 -0500

    Do local dummy fetch for frontend testing

--- rate-my-housing/app/page.tsx
+++ rate-my-housing/app/page.tsx
@@ -5,33 +5,29 @@ import Stack from '@mui/material/Stack';
 import Card from '@mui/material/Card';
 import SearchResultsLayout from '@/components/SearchResultsLayout';
 
-async function getUsers() {
-  const res = await fetch('http://localhost:8080/users')
+const places = [
+  {
+    complex_id: "id",
+    name: "Test place",
+    management: "managers",
+  },
 
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  return res.json()
-}
+]
 
 async function getComplexes() {
-  const res = await fetch('http://localhost:8080/complexes')
+  /* const res = await fetch('http://localhost:8080/complexes')
 
   if (!res.ok) {
     throw new Error('Failed to fetch data')
   }
 
-  return res.json()
+  return res.json() */
+  return places
 }
-
-// !!!
-const ENABLE_API = false;
-// !!!
 
 export default async function Home() {
 
-  let complexes = ENABLE_API ? await getComplexes() : [];
+  let complexes = await getComplexes();
   
   return (
     <>
