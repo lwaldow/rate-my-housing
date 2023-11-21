1e80d08be9a2fd3cee896569274383745464fe70
Jasmine Ngo <jasvngo@gmail.com>
2023-11-21 15:55:51 -0500

    add picture for card

--- rate-my-housing/components/SearchListingCard.tsx
+++ rate-my-housing/components/SearchListingCard.tsx
@@ -25,23 +25,32 @@ export default function RootLayout({ complex }: { complex: Complex }) {
         lineHeight: '1.5'
       };
       
+      const blankImageStyle = {
+        width: '350px',
+        height: '135px',
+        marginRight: '16px',
+        border: '3px solid gray',
+    };
 
     return (
         <Card key={complex.complex_id}>
-            <CardContent sx={{ cursor: 'pointer', mb: 14 }}>
+            <CardContent sx={{ cursor: 'pointer', mb: 1 }}>
                 <div className='text-lg flex justify-between'>
                 <span><b style={{fontFamily: 'Playfair Display, serif',fontWeight: 800, fontSize: 25}}>{complex.name + ' '}</b></span>
-                    <Box component="fieldset" mb={3} borderColor="transparent">
+                    <Box component="fieldset" mb={1} borderColor="transparent">
                         <Rating name="read-only" value={complex.stars} readOnly />
                     </Box>
                 </div>
-                <Typography sx={{ fontSize: 16, color: 'gray' }}>{complex.address}</Typography>
-                <Typography sx={{ fontSize: 16 }}>{complex.management}</Typography>
+                <Typography sx={{ fontSize: 16, color: 'gray', margintop: 1 }}>{complex.address}</Typography>
+                <Typography sx={{ fontSize: 16}}>{complex.management}</Typography>
                 <Typography sx={{ fontSize: 16 }}>{"ID: " + complex.complex_id}</Typography>
+                <img src="/path/to/blank-white-image.png" alt="Blank White" style={blankImageStyle} />
             </CardContent>
             <CardActions>
+            <div style={{ marginLeft: 'auto' }}>
                 <Link href={`/listings/${complex.complex_id}/`}style={buttonStyle}>About Me</Link>
                 <Link href={`/listings/${complex.complex_id}/rate`}style={buttonStyle}>Rate Me</Link>
+            </div>
             </CardActions>
         </Card>
     )
